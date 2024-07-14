abdf6b3a72d22282094b729bab527697caeedc04
mwtmurphy <mwtmurphy@gmail.com>
2024-07-14 01:01:28 +0100

    refactor: experiment with logreg modelling added

--- src/f1_elo/model.py
+++ src/f1_elo/model.py
@@ -6,7 +6,7 @@ import yaml
 import bayes_opt
 import numpy as np
 import pandas as pd
-
+from sklearn import linear_model
 
 # global variables
 with open("params.yaml") as conf_file:
@@ -20,14 +20,17 @@ MOD_MAT = MOD_DF.values
 DRI_RTG = {dri: CONFIG["model"]["start_score"] for dri in set(MOD_DF["driverId"])}
 CON_RTG = {con: CONFIG["model"]["start_score"] for con in set(MOD_DF["constructorId"])}
 
-def model_data(k: float, c: float, w: float, export: bool = False) -> typing.Union[float, None]:
+def model_data(k: float, s: float, w: float, prob_model = None) ->pd.DataFrame:
     '''If export == False, returns negative RMSEE based on params. 
     If export == True, exports modelled data to 'interim' data folder 
     for data reporting.'''
 
     dri_scores = DRI_RTG.copy()
     con_scores = CON_RTG.copy()
-    exp, out = [], []
+    rating_diffs = []
+    true_outcomes = []
+    log_losses = []
+    pred_errors = []
 
     for start_ix, end_ix in IX_CHUNKS:
         yr_mat = MOD_MAT[start_ix:end_ix+1]
@@ -39,28 +42,34 @@ def model_data(k: float, c: float, w: float, export: bool = False) -> typing.Uni
             con_b, dri_b, pos_b, st_b = yr_mat[ix_2, [3, 4, 5, 7]]
     
             # continue if drivers in same car or a driver does not finish for misc reason
-            if pos_a == pos_b or "misc retirement" in [st_a, st_b]:
+            if pos_a == pos_b or st_a != "finished" or st_b != "finished":
                 continue
 
-            # get current rating
-            elo_a = dri_scores[dri_a] + (w * con_scores[con_a])
-            elo_b = dri_scores[dri_b] + (w * con_scores[con_b])
-            
-            # calculate position influence
-            q_a = 10 ** (elo_a / c)
-            q_b = 10 ** (elo_b / c)
-    
-            e_a = q_a / (q_a + q_b)        
-            e_b = q_b / (q_a + q_b)
+            # get expected score
+            r_a = dri_scores[dri_a] + (w * con_scores[con_a])
+            r_b = dri_scores[dri_b] + (w * con_scores[con_b])
+            diff_r = r_a - r_b
 
-            # score outcome
+            e_a = float(prob_model.predict_proba(np.array(diff_r).reshape(1, -1))[0, 1]) if prob_model else 1 / (1 + 10 ** (-diff_r / s))
+            e_b = 1 - e_a
+
+            # get outcome score
             if pos_a < pos_b:
                 o_a = 1
                 o_b = 0
+                log_losses.append(-np.log(e_a))
+
             else:
                 o_a = 0
                 o_b = 1
+                log_losses.append(-np.log(e_b))
                 
+            pred_errors.append(o_a - e_a)
+            
+            # store outcomes for log reg model
+            rating_diffs += [diff_r, -diff_r]
+            true_outcomes += [o_a, o_b]
+
             # calculate score change and update round scores
             diff_a = k * (o_a - e_a)
             diff_b = k * (o_b - e_b)
@@ -79,15 +88,11 @@ def model_data(k: float, c: float, w: float, export: bool = False) -> typing.Uni
             
             # log constructor changes if diff constructors and neither driver retires due to driver error (not attributable to constructors)
             if con_a != con_b and "driver retirement" not in [st_a, st_b]:
-                rnd_con_scores[con_a]["diff"] += diff_a
+                rnd_con_scores[con_a]["diff"] += w * diff_a
                 rnd_con_scores[con_a]["n"] += 1
     
-                rnd_con_scores[con_b]["diff"] += diff_b
+                rnd_con_scores[con_b]["diff"] += w * diff_b
                 rnd_con_scores[con_b]["n"] += 1
-                
-            # store expected and final values for error analysis
-            exp += [e_a, e_b]
-            out += [o_a, o_b]
         
         # update driver values for finishing drivers and driver-caused retirements
         for dri in rnd_dri_scores.keys():
@@ -104,42 +109,49 @@ def model_data(k: float, c: float, w: float, export: bool = False) -> typing.Uni
                 con_scores[con] += (rnd_con_scores[con]["diff"] / rnd_con_scores[con]["n"])
         
         yr_mat[:, 8] = list(map(lambda el: con_scores[el], yr_mat[:, 3]))
-
-    if export == False:
-        err_df = pd.DataFrame({"pred": exp, "true": out})
-        err_df["squared_error"] = (err_df["true"] - err_df["pred"]) ** 2
-        neg_rmse = -(pow(err_df["squared_error"].sum() / err_df.shape[0], 0.5))
         
-        return neg_rmse
-    
-    else:
-        RES_DF = pd.DataFrame(MOD_MAT, columns=MOD_DF.columns)
-        RES_DF.to_csv(CONFIG["data"]["modelled_path"], index=False)
+    # print summary errors
+    rmse = np.sqrt(np.mean(np.square(pred_errors)))
+    mean_loss = np.mean(log_losses)
+
+    return rmse, mean_loss, rating_diffs, true_outcomes
         
 
 if __name__=="__main__":
+    K = 32
+    S = 400
+    W = 1.25
 
-    # find optimal parameters
-    opt_params = CONFIG["model"]["opt_params"]
-    optimiser = bayes_opt.BayesianOptimization(
-        f=model_data,
-        pbounds=opt_params["pbounds"],
-        random_state=opt_params["random_state"],
-        allow_duplicate_points=True
-    )
+    for K in [22, 32, 42]:
+        for S in [300, 400, 500]:
+            for W in [0.9, 1, 1.1]:
 
-    optimiser.maximize(
-        init_points=opt_params["init_points"], 
-        n_iter=opt_params["n_iter"]
-    )
-    results = optimiser.max
+                _, mean_loss, x, y = model_data(K, S, W)
+                prev_mean_loss = 1
+                n = 2
+                prob_model = linear_model.LogisticRegression()
+
+                while n <= 40 and prev_mean_loss - mean_loss >= 0.0001:
+                    prev_mean_loss = mean_loss
+                    x = np.array(x).reshape(-1, 1)
+                    prob_model.fit(x, y)
+                    rmse, mean_loss, x, y = model_data(K, S, W, prob_model=prob_model)
+                    n += 1
+
+                print(f"[*] K={K}, S={S}, W={W}, RMSE={round(rmse, 4)}, MLOSS={round(mean_loss, 4)}")
+
+
+    RES_DF = pd.DataFrame(MOD_MAT, columns=MOD_DF.columns)
+    RES_DF.to_csv(CONFIG["data"]["modelled_path"], index=False)
+
     params_log = {
-        "k": float(results["params"]["k"]),
-        "c": float(results["params"]["c"]),
-        "w": float(results["params"]["w"])
+        "k": K,
+        "s": S,
+        "w": W
     }
     metrics_log = {
-        "RMSE": float(-results["target"])
+        "RMSE": rmse,
+        "mean_loss": mean_loss
     }
 
     #log metrics and params locally
@@ -148,6 +160,3 @@ if __name__=="__main__":
 
     with open(CONFIG["data"]["params_path"], "w") as out:
         yaml.dump(params_log, out)
-
-    # create final model data
-    model_data(k=params_log["k"], c=params_log["c"], w=params_log["w"], export=True)
